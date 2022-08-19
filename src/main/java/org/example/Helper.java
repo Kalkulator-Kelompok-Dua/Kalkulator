aeb9097571ba272fdc69f32cf30ed8831662009f
dwikifadhilah <dwkfadhilah@gmail.com>
2022-08-19 14:44:06 +0700

    Using OOP and Add Button Zero

--- src/main/java/org/example/Helper.java
+++ src/main/java/org/example/Helper.java
@@ -1,11 +1,33 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 class Helper {
-    protected WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
     public Helper(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+    }
+
+    public void click(By by){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        driver.findElement(by).click();
+    }
+
+    public String getText(By by){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        return driver.findElement(by).getText();
+    }
+
+    public boolean verifyElement(By by){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        return driver.findElement(by).isDisplayed();
     }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -2,33 +2,35 @@ package org.example;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CalculatorTest extends BaseTest {
 
     @Test
     public void clearEntryTest(){
 
-        PageObject page = new PageObject(driver);
+        Assert.assertTrue(calculatorPage.isDisplayed());
 
-        Assert.assertEquals("0", page.getMainScreenText());
+        calculatorPage
+                .clickButtonOne()
+                .clickButtonTwo()
+                .clickButtonThree()
+                .clickButtonFour()
+                .clickButtonFive()
+                .clickButtonSix()
+                .clickButtonSeven()
+                .clickButtonEight()
+                .clickButtonNine()
+                .clickButtonZero();
 
-        driver.findElement(page.buttonOne).click();
-        driver.findElement(page.buttonTwo).click();
-        driver.findElement(page.buttonThree).click();
-        driver.findElement(page.buttonFour).click();
-        driver.findElement(page.buttonFive).click();
-        driver.findElement(page.buttonSix).click();
-        driver.findElement(page.buttonSeven).click();
-        driver.findElement(page.buttonEight).click();
-        driver.findElement(page.buttonNine).click();
+        Assert.assertEquals("1234567890", calculatorPage.getMainScreenText());
 
-        Assert.assertEquals("123456789", page.getMainScreenText());
+        calculatorPage
+                .clickButtonClearEntry()
+                .clickButtonClearEntry()
+                .clickButtonClearEntry();
 
-        driver.findElement(page.buttonClearEntry).click();
-        driver.findElement(page.buttonClearEntry).click();
+        Assert.assertEquals("1234567", calculatorPage.getSubScreenText());
+        Assert.assertEquals("1234567", calculatorPage.getMainScreenText());
 
-        Assert.assertEquals("1234567", page.getSubScreenText());
-        Assert.assertEquals("1234567", page.getMainScreenText());
     }
 }
